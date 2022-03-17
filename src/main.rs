b773cd188e6342a14a091c629bbb2c80c0b956a7
Janamajaya Mall <janmajaya.choithram@gmail.com>
2022-03-18 00:32:34 +0800

    update behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use async_std::io;
-use libp2p::kad::{GetProvidersOk, Kademlia, KademliaEvent, QueryId, QueryResult};
+use libp2p::kad::{GetProvidersOk, Kademlia, KademliaEvent, QueryId, QueryResult, KademliaConfig, KadConnectionType};
 use libp2p::kad::record::store::MemoryStore;
-use libp2p::{NetworkBehaviour};
+use libp2p::{NetworkBehaviour, PeerId};
 use libp2p::swarm::{NetworkBehaviour, NetworkBehaviourEventProcess};
 use libp2p::identity::{Keypair};
 use std::error::Error;
+use std::time::Duration;
 
 #[derive(NetworkBehaviour)]
 #[behaviour(event_process = true)]
@@ -18,6 +19,23 @@ impl NetworkBehaviourEventProcess<KademliaEvent> for Behaviour {
     }
 }
 
+impl Behaviour {
+    pub async fn new(peer_id: PeerId)  {
+        // setup kademlia
+        let store = MemoryStore::new(peer_id);
+        let mut kad_config = KademliaConfig::default();
+        kad_config.set_protocol_name("kad_protocol".as_bytes());
+        kad_config.set_query_timeout(Duration::from_secs(300));
+        // TODO set disjoint_query_paths to true, once you understand this -https://discuss.libp2p.io/t/s-kademlia-lookups-over-disjoint-paths-in-rust-libp2p/571
+        let kademila = Kademlia::with_config(peer_id, store, kad_config);
+
+        // TODO supply and add bootstrap nodes
+        
+        
+
+    }    
+}
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // create keypair for the node
@@ -25,6 +43,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let peer_id = keypair.public().to_peer_id();
 
     // connect
+
+
 
     Ok(())
 

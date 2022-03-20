efdef868a4e9ddd82d78d6ddaad2a0922a3f248f
janmajayamall <janmajaya.choithram@gmail.com>
2022-03-20 13:19:19 +0000

    added bootnode opts

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use async_std::{self};
 use async_std::prelude::StreamExt;
 use libp2p::core::transport::Boxed;
 use libp2p::futures::stream::Peek;
-use libp2p::kad::{GetProvidersOk, Kademlia, KademliaEvent, QueryId, QueryResult, KademliaConfig, KadConnectionType};
+use libp2p::kad::{GetProvidersOk, Kademlia, KademliaEvent, QueryId, QueryResult, KademliaConfig, KadConnectionType, BootstrapOk, GetClosestPeersOk, GetClosestPeersError, BootstrapError};
 use libp2p::kad::record::store::MemoryStore;
-use libp2p::{NetworkBehaviour, PeerId, Transport, Multiaddr};
+use libp2p::{NetworkBehaviour, PeerId, Transport, Multiaddr, Swarm};
 use libp2p::swarm::{NetworkBehaviour, NetworkBehaviourEventProcess, SwarmBuilder, SwarmEvent};
-use libp2p::identity::{Keypair};
+use libp2p::identity::{Keypair, secp256k1};
 use libp2p::dns::TokioDnsConfig;
 use libp2p::tcp::TokioTcpConfig;
 use libp2p::core::transport::upgrade::Version;
@@ -26,8 +26,17 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "DSE args")]
 struct Opt {
-    boot_id: Option<String>,
-    boot_addr: Option<String>
+    #[structopt(long)]
+    seed: Option<u8>,
+
+    #[structopt(long)]
+    listen_on: Option<Multiaddr>,
+
+    #[structopt(long)]
+    boot_id: Option<PeerId>,
+
+    #[structopt(long)]
+    boot_addr: Option<Multiaddr>
 }
 
 #[derive(NetworkBehaviour)]
@@ -48,6 +57,21 @@ impl NetworkBehaviourEventProcess<KademliaEvent> for Behaviour {
             }
             KademliaEvent::OutboundQueryCompleted { id, result, stats } => {
                 println!("kad: outboud query completed for query id {:?} and result {:?} ", id, result);
+                match result {
+                    QueryResult::Bootstrap(Ok(BootstrapOk { peer, num_remaining })) => {
+                        println!("kad: bootstrapped with peer: {:?}; remaining peer count {} ", peer, num_remaining);
+                    },
+                    QueryResult::Bootstrap(Err(BootstrapError::Timeout{peer, num_remaining})) => {
+                        println!("kad: timedout while bootstrapping");
+                    }
+                    QueryResult::GetClosestPeers(Ok(GetClosestPeersOk{key, peers})) => {
+                        println!("kad: closest peers with key {:?}: {:?}", key, peers);
+                    },
+                    QueryResult::GetClosestPeers(Err(GetClosestPeersError::Timeout{key, peers})) => {
+                        println!("kad: timedout while getting closest peer to key {:?} ", key);
+                    },
+                    _ => {},
+                }
             }
             _ => {}
         }
@@ -118,8 +142,7 @@ impl NetworkBehaviourEventProcess<PingEvent> for Behaviour {
 
 
 impl Behaviour {
-    pub async fn new(peer_id: PeerId) -> Self  {
-        let opt = Opt::from_args();
+    pub async fn new(peer_id: PeerId, boot_id: Option<PeerId>, boot_addr: Option<Multiaddr>) -> Self  {
 
         // setup kademlia
         let store = MemoryStore::new(peer_id);
@@ -136,11 +159,12 @@ impl Behaviour {
         let ping = Ping::new(ping_config);
 
         // add bootnode
-        if let (Some(boot_id),  Some(boot_addr)) = (opt.boot_id, opt.boot_addr) {
-            println!("Bootnode peerId {:?} multiAddr {:?} ", boot_id ,boot_addr);
-            kademlia.add_address(&boot_id.parse().expect("Bootnode Peer Id"), boot_addr.parse().expect("Bootnode Address"));
+        if let (Some(boot_id),  Some(boot_addr)) = (boot_id, boot_addr) {
+            println!("Bootnode peerId {:?} multiAddr {:?} ", boot_id, boot_addr);
+            kademlia.add_address(&boot_id, boot_addr);
         }
 
+    
         Behaviour {
             kademlia,
             ping,
@@ -181,15 +205,26 @@ impl libp2p::core::Executor for CustomExecutor {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
+    let opt = Opt::from_args();
+
     // create keypair for the node
-    let keypair = Keypair::generate_secp256k1();
+    let keypair = match opt.seed {
+        Some(seed) => {
+            let mut bytes = [0u8; 32];
+            bytes[0] = seed;
+            let secret_key = secp256k1::SecretKey::from_bytes(bytes).expect("Invalid seed");
+            Keypair::Secp256k1(secret_key.into())
+        },
+        None => Keypair::generate_secp256k1(),
+    };
+
     let peer_id = keypair.public().to_peer_id();
 
     println!("Node peer id {:?} ", peer_id.to_base58());
 
     // Build swarm
     let transport = build_transport(&keypair)?;
-    let behaviour = Behaviour::new(peer_id).await;
+    let behaviour = Behaviour::new(peer_id, opt.boot_id, opt.boot_addr).await;
     let mut swarm = SwarmBuilder::new(
         transport,
         behaviour,
@@ -199,8 +234,12 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     }).build();
 
 
-    // Listen on all interfaces and whatever port the OS assigns.
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    // Listen on either provided opt value or any interface
+    if let Some(listen_on) = opt.listen_on {
+        swarm.listen_on(listen_on)?;
+    }else {
+        swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    }
 
     // bootstrap node
     match swarm.behaviour_mut().kademlia.bootstrap() {
@@ -221,8 +260,12 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
             event = swarm.next() => {
                 println!("Swarm event {:?} ", event);
             }
-            line = stdin.next() => {
-                println!("line {:?}", line.expect("Line"));
+            line = stdin.next() => {    
+                if let Some(_line) = line {
+                    let val = _line.unwrap();
+                    println!("line {}", val);
+                    handle_input(val, &mut swarm).await;
+                }
             }
         }
     }
@@ -230,6 +273,31 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-async fn handle_input() {
-    
+async fn handle_input(command: String, swarm: &mut Swarm<Behaviour>) {
+    let mut args = command.split(" ");
+
+    match args.next() {
+        Some("BOOTSTRAP") => {
+            match swarm.behaviour_mut().kademlia.bootstrap() {
+                Ok(id) => {
+                    println!("Bootstrapping with query {:?} ", id);
+                },
+                Err(e) => {
+                    println!("No known peers for bootstrapping");
+                }
+            }
+        },
+        Some("CLOSEST_PEERS") => {
+            if let Some(id) = args.next() {
+                let peer_id: PeerId = id.parse().unwrap();
+                let query_id = swarm.behaviour_mut().kademlia.get_closest_peers(peer_id);
+                println!("Query id for closest peers to id {:?}: {:?} ", id, query_id);
+            }else {
+                println!("Peer id not provided");
+            }
+        },
+        _ => {
+            println!("Unrecognised command!");
+        }
+    }
 }

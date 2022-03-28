526402ce424b9aad6c6a3f7d962b54886349ddc3
janmajayamall <janmajaya.choithram@gmail.com>
2022-03-28 22:04:31 +0000

    adding contracts

---
+++ contracts/src/Contract.sol
@@ -0,0 +1,23 @@
+contract TimeLockedWallet {
+
+    uint epochDuration;
+    address owner;
+
+    constructor(
+        uint _epochDuration,
+        address _owner,
+    ) {
+        epochDuration = _epochDuration;
+        
+    }
+
+    // need a way to deposit
+    // 50% go to wallet balance & 50% for security deposit
+
+    // owner 
+
+    // epoch & start the epoch 
+
+    // 
+
+} 

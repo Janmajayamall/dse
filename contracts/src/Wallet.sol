acb66f0b6b2c8df269d73e9e20099711bc2c7c12
janmajayamall <janmajaya.choithram@gmail.com>
2022-04-01 07:53:51 +0000

    updated commit redeem and burn to only calldata

--- contracts/src/Wallet.sol
+++ contracts/src/Wallet.sol
@@ -47,6 +47,8 @@ contract Wallet {
     // signature.
     uint constant invDisadvantageBuffer = 1 hours;
 
+    uint constant ECDSA_SIGNATURE_LENGTH = 65;
+
     error BalanceError();
     error NotOwner();
     error EpochOngoing();
@@ -64,6 +66,7 @@ contract Wallet {
         currency = _currency;
         burnBuffer = _burnBuffer;
     }
+
 
     function getBalance(
         address token
@@ -84,6 +87,20 @@ contract Wallet {
             IERC20(currency).transfer(address(0), _pendingAmount);
             pendingBurnAmount = 0;
         }
+    }
+
+    function recoverSigner(uint sIndex, bytes32 message) internal pure returns (address) {
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := calldataload(sIndex)
+            s := calldataload(add(sIndex, 32))
+            v := shr(248, calldataload(add(sIndex, 32)))
+        }
+
+        return ecrecover(message, v, r, s);
     }
 
     /// Checks whether commit of an index
@@ -149,29 +166,19 @@ contract Wallet {
     }
 
     /// Redeem time-locked commitments of type 2 
-    function redeemdType2(
-        uint32[] calldata indexes,
-        uint32[] calldata u,
-        uint8[] calldata v,
-        bytes32[] calldata r,
-        bytes32[] calldata s,
-        uint8[] calldata vi,
-        bytes32[] calldata ri,
-        bytes32[] calldata si,
-        address to
-    ) public {
-        // sanity checks
-        if (block.timestamp >= epochExpiresBy) revert();
-        if (indexes.length != v.length) revert();
-        if (
-            v.length != u.length ||
-            v.length != r.length || 
-            v.length != s.length ||
-            v.length != vi.length ||
-            v.length != ri.length ||
-            v.length != si.length ||
-            v.length != vi.length
-        ) revert();
+    /// Fn selector - 4 bytes
+    /// Index count - 2 bytes
+    /// To Address - 20 bytes
+    /// Data per index - 139 bytes
+    ///     Index(4 bytes) + U(4 bytes) + CommitSignature(65 bytes)  + InvalidatingSignature(65 bytes )
+    function redeemdType2() public {
+
+        uint16 count;
+        address toAddress;
+        assembly {
+            count := shr(240, calldataload(4))
+            toAddress := shr(96, calldataload(6))
+        }
 
         uint256 _indexValue = indexValue;
         uint256 _currentEpoch = currentEpoch;
@@ -180,77 +187,90 @@ contract Wallet {
         // total amount to be redeemed
         uint256 totalAmount;
 
-        for (uint256 i = 0; i < indexes.length; i++) {
+        for (uint256 i = 0; i < uint256(count); i++) {
+
+            uint32 index;
+            uint32 u;
+            uint byteOffset = 26 + (139 * i); // 139 = 4 + 4 + 65 + 65
+            assembly {  
+                index := shr(224, calldataload(byteOffset))
+                u := shr(224, calldataload(add(byteOffset, 4)))
+            }
+
             // check that index for the epoch hasn't been claimed 
             // nor pending burn
-            bytes32 claimIdentifier = keccak256(abi.encodePacked(indexes[i], _currentEpoch));
+            bytes32 claimIdentifier = keccak256(abi.encodePacked(index, _currentEpoch));
             if (
                 burnBufferTime[claimIdentifier] != 0 ||
                 claimed[claimIdentifier] == true
             ) revert();
 
             bytes32 commit = keccak256(abi.encodePacked(
-                uint256(indexes[i]), 
+                uint256(index), 
                 uint256(_currentEpoch), 
-                uint256(u[i]), 
+                uint256(u), 
                 uint256(2), 
                 _owner, 
-                to
+                toAddress
             ));
 
             // checks that owner signed the commitment
-            if (ecrecover(commit, v[i], r[i], s[i]) != _owner) revert();
+            if (recoverSigner(byteOffset + 8, commit) != _owner) revert();
 
             // check that owner signed the invalidating message
-            bytes32 iCommit = keccak256(abi.encodePacked(u[i]));
-            if (ecrecover(iCommit, vi[i], ri[i], si[i]) != _owner) revert();
+            if (recoverSigner(byteOffset + 73, keccak256(abi.encodePacked(u))) != _owner) revert();
 
             totalAmount += _indexValue;
 
             claimed[claimIdentifier] = true;
         }
 
-        IERC20(currency).transfer(to, totalAmount);
+        IERC20(currency).transfer(toAddress, totalAmount);
 
         // TODO emit claimType2
     }
 
     /// Burn commitments with no invalidating
     /// signatures
-    /// Note that arrays are split into two 
-    /// parts defined by splitIndex.
-    /// First part is for type 1 commits
-    /// Second part is for type 2 commits
-    function burnCommits(
-        uint32[] calldata indexes,
-        uint32[] calldata u,
-        uint8[] calldata v,
-        bytes32[] calldata r,
-        bytes32[] calldata s,
-        address type1IAddress,
-        address type2ToAddress,
-        uint256 splitIndex
-    ) public {
+    /// Fn selector - 4 bytes
+    /// Index count - 2 bytes
+    /// ToAddress - 20 bytes
+    /// Data per index - 74 bytes
+    ///     Index(4 bytes) + U(4 bytes) + Type(1 byte) + CommitSignature(65 bytes)
+    ///     Type - 
+    ///         (1) CType 1 and IAddress = Owner Address
+    ///         (2) CType 1 and IAddress = To Address
+    ///         (3) CType 2 (IAddress is owner address by default)
+    function burnCommits() public {
         uint _burnBuffer = burnBuffer;
 
         // Can't call burn after epochExpiry - burnBuffer - invDisadvantageBuffer.
         if (block.timestamp >= epochExpiresBy - (_burnBuffer + invDisadvantageBuffer)) revert();
 
-        // sanity checks
-        if (indexes.length != v.length) revert();
-        if (
-            v.length != r.length || 
-            v.length != s.length ||
-            v.length != u.length
-        ) revert();
+        uint16 count;
+        address toAddress;
+        assembly {
+            count := shr(240, calldataload(4))
+            toAddress := shr(96, calldataload(6))
+        }
 
         uint256 _indexValue = indexValue;
         uint256 _currentEpoch = currentEpoch;
         address _owner = owner;
         uint256 totalBurn;
 
-        for (uint256 i = 0; i < v.length; i++) {
-            bytes32 claimIdentifier = keccak256(abi.encodePacked(indexes[i], _currentEpoch));
+        for (uint256 i = 0; i < count; i++) {
+            uint32 index;
+            uint32 u;
+            uint8 _type;
+            uint byteOffset = 26 + (74 * i); // 74 = 4 + 4 + 1 + 65
+            assembly {  
+                index := shr(224, calldataload(byteOffset))
+                u := shr(224, calldataload(add(byteOffset, 4)))
+                _type := shr(248, calldataload(add(byteOffset, 8)))
+            } 
+
+            bytes32 claimIdentifier = keccak256(abi.encodePacked(index, _currentEpoch));
             // check that index for the epoch hasn't been claimed 
             // nor pending burn
             if (
@@ -258,26 +278,27 @@ contract Wallet {
                 claimed[claimIdentifier] == true
             ) revert();
 
-
             bytes32 commit;
             uint256 bTime = _burnBuffer;
-            if (i < splitIndex){
+            address _toAddress = toAddress;
+            if (_type < 2){
+                address iAddress = _type == 0 ? _owner : toAddress;
                 // type 1 commits
                 commit = keccak256(
                     abi.encodePacked(
-                        uint256(indexes[i]), 
+                        uint256(index), 
                         uint256(_currentEpoch), 
-                        uint256(u[i]), 
+                        uint256(u), 
                         uint256(1), 
-                        type1IAddress
+                        iAddress
                     )
                 );
 
-                burnBufferIAddress[claimIdentifier] = type1IAddress;
+                burnBufferIAddress[claimIdentifier] = iAddress;
 
                 // if owner isn't iAddress
                 // add invDisadvantageTime to buffer time
-                if (_owner != type1IAddress){
+                if (_type == 1){
                     bTime += invDisadvantageBuffer;
                 }
             }else {
@@ -286,22 +307,22 @@ contract Wallet {
                 // onwers themselves by default
                 commit = keccak256(
                     abi.encodePacked(
-                        uint256(indexes[i]), 
+                        uint256(index), 
                         uint256(_currentEpoch), 
-                        uint256(u[i]),  
+                        uint256(u),  
                         uint256(2), 
-                        _owner, 
-                        type2ToAddress
+                        _owner,
+                        _toAddress
                     )
                 );
                 burnBufferIAddress[claimIdentifier] = _owner;
             }
 
             // checks that owner signed the commitment
-            if (ecrecover(commit, v[i], r[i], s[i]) != _owner) revert();
+            if (recoverSigner(byteOffset + 9, commit) != _owner) revert();
 
             burnBufferTime[claimIdentifier] = bTime;
-            burnBufferIBlob[claimIdentifier] = keccak256(abi.encodePacked(u[i]));
+            burnBufferIBlob[claimIdentifier] = keccak256(abi.encodePacked(u));
 
             totalBurn += _indexValue;
         }
